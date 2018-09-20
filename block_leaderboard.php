67a6aeba9425479403d4a2b8ba9f1500539eebd5
efox12 <erikfox@Eriks-MacBook-Pro-3.local>
2018-09-20 12:06:14 -0700

    Now adds points retrospectively

--- block_leaderboard.php
+++ block_leaderboard.php
@@ -31,6 +31,47 @@ class block_leaderboard extends block_base {
         $renderer = $this->page->get_renderer('block_leaderboard');
         $this->content->text = $renderer->leaderboard_block($this->page->course);
         
+        //updates points values for assignments retrospectively
+        global $DB;
+        $groups = $DB->get_records('groups');
+        $all_assignments = $DB->get_records('assign');
+        foreach($groups as $group){
+            //get each member of the group
+            $students = groups_get_members($group->id, $fields='u.*', $sort='lastname ASC');
+            foreach($students as $student){
+                $submission_data = $DB->get_records('assign_submission',array('userid'=> $student->id));
+                $keys = array_keys($submission_data);
+                foreach($keys as $key){
+                    $assignment_data_key = $submission_data[$key]->assignment;
+                    $assignment_data = $all_assignments[$assignment_data_key];
+                    $due_date = $assignment_data->duedate;
+                    $assignment_table = $DB->get_record('assignment_table', array('activity_student'=> $student->id, 'activity_id' => $key));
+                    if($assignment_table != false){
+                        $days_early = ($due_date - $assignment_table->time_finished)/86400;
+                        if($assignment_table->points_earned == 0 && $days_early > 0){
+                            $points = 0;
+                            for($x=1; $x<=5; $x++){
+                                $current_time = get_config('leaderboard','assignmenttime'.$x);
+                                if($x < 5) {
+                                    $next_time = get_config('leaderboard','assignmenttime'.($x+1));
+                                    if($days_early >= $current_time && $days_early < $next_time){
+                                        $points = get_config('leaderboard','assignmnetpoints'.$x);
+                                        break;
+                                    }
+                                } else {
+                                    if($days_early >= $current_time){
+                                        $points = get_config('leaderboard','assignmnetpoints'.$x);
+                                        break;
+                                    }
+                                }
+                            }
+                            $assignment_table->points_earned = block_leaderboard_multiplier::calculate_points($student->id, $points);
+                            $DB->update_record('assignment_table', $assignment_table);
+                        }
+                    }
+                }
+            }
+        }
         return $this->content;
     }
 
