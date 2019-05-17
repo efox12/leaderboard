c528e67609a1d6780f9cae2c3f6e6b0c0c12f806
Erik Fox <efox.rj@gmail.com>
2019-05-17 10:51:00 -0700

    version bump

--- version.php
+++ version.php
@@ -5,7 +5,7 @@
  * Last Updated: 12/29/18
  */
 
-$plugin->version   = 2019091800;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2019091801;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->release = 'v2.0';
 $plugin->maturity = MATURITY_STABLE;
