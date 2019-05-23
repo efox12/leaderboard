a63d66d9372884615a64a3185b6f6326d92c0700
Erik Fox <efox.rj@gmail.com>
2019-05-22 19:05:46 -0700

    test fix pt 2

--- db/upgrade.php
+++ db/upgrade.php
@@ -86,7 +86,7 @@ function xmldb_block_leaderboard_upgrade($oldversion) {
         upgrade_block_savepoint(true, 2019091840, 'leaderboard');
 
     }
-    if ($oldversion < 2019091841) {
+    if ($oldversion < 2019091842) {
         $table = new xmldb_table('block_leaderboard_quiz');
         $field = new xmldb_field('days_spaced', XMLDB_TYPE_NUMBER, '10, 5', null, null, null, null, 'days_early');
 
@@ -94,7 +94,7 @@ function xmldb_block_leaderboard_upgrade($oldversion) {
         $dbman->change_field_type($table, $field);
 
         // Leaderboard savepoint reached.
-        upgrade_block_savepoint(true, 2019091841, 'leaderboard');
+        upgrade_block_savepoint(true, 2019091842, 'leaderboard');
     }
     return true;
 
