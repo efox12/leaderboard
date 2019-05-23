cc2b24703239f17ed464386f174d2a350554ed17
Erik Fox <efox.rj@gmail.com>
2019-05-22 18:43:17 -0700

    version bump

--- version.php
+++ version.php
@@ -22,7 +22,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019091801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2019091840;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018050800;        // Requires this Moodle version.
 $plugin->release = 'v2.0';
 $plugin->maturity = MATURITY_STABLE;
