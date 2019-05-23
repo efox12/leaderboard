252ab69726697394e493dbd94e47b7bcd0229012
Erik Fox <efox.rj@gmail.com>
2019-05-22 18:47:22 -0700

    db 4

--- db/upgrade.php
+++ db/upgrade.php
@@ -36,7 +36,7 @@ function xmldb_block_leaderboard_upgrade($oldversion) {
 
     // Add a new column newcol to the mdl_myqtype_options
     if ($oldversion < 2019091840) {
-        /*
+
         // Define table block_leaderboard_assignment to be renamed to NEWNAMEGOESHERE.
         $table = new xmldb_table('assignment_table');
         // Launch rename table for block_leaderboard_assignment.
@@ -58,7 +58,7 @@ function xmldb_block_leaderboard_upgrade($oldversion) {
 
         // Launch rename table for group_data_table.
         $dbman->rename_table($table, 'block_leaderboard_group_data');
-        */
+        /*
         $table = new xmldb_table('group_data_table');
 
         $field = new xmldb_field('prevoiusstanding', XMLDB_TYPE_INTEGER, '10', null, null, null, null, 'current_standing');
@@ -84,6 +84,7 @@ function xmldb_block_leaderboard_upgrade($oldversion) {
 
         // Leaderboard savepoint reached.
         //upgrade_block_savepoint(true, 2019091830, 'leaderboard');
+        */
     }
     return true;
 
